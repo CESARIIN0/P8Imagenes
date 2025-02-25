720e703589c10093ef262023eb101148a18eff33
Cesar_Reyes_V <cesarreyesv2022@gmail.com>
2025-02-25 17:03:18 +0000

    cuchillo

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Mis imagenes",
+            style: TextStyle(
+              fontSize: 16,
+              fontStyle: FontStyle.italic,
+              color: Color(0xff000000),
+            ),
+          ),
+          backgroundColor: Color(0xff01d25f),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Cesar Reyes Vazquez 22308051281294',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xff2ac55d),
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/CESARIIN0/imagenes-para-APP-flutter-6.-j/refs/heads/main/cuchillo3.jpg',
+                width: 200, // Ancho opcional
+                height: 200, // Alto opcional
+              ),
+              SizedBox(height: 20), // Espacio entre las imágenes
+              Text(
+                'Logo de cuchillo',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey[600],
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/CESARIIN0/imagenes-para-APP-flutter-6.-j/refs/heads/main/cuchillo2.jpg',
+                width: 200, // Ancho opcional
+                height: 200, // Alto opcional
+              ),
+              SizedBox(height: 20), // Espacio entre las imágenes
+              Text(
+                'comida',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey[600],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ); // Material app
+  } // widgets
+} // clase mi imagen
